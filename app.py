ea5752a61195744664d36a082432b4c53f9d1261
rhysseddon <rhysseddon@hotmail.com>
2021-01-19 17:25:41 +0000

    Add Logout functionality

--- app.py
+++ app.py
@@ -60,21 +60,22 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # invalid password match
-                flash("Incorrect Username and/or Password")   
+                flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-            #  user doesnt exist
+            # username doesn't exist
             flash("Incorrect Username and/or Password")
-            return redirect(url_for("login"))    
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
@@ -83,7 +84,18 @@ def profile(username):
     # grab the session users username from the db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
